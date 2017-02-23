dd3c3c07f76f60bac35342b67bff9eaee75f7470
lyleighen <l.leigh.hendry@gmail.com>
2017-02-23 15:47:54 +0000

    Prevent bubbles spawning too close

--- bubbles.js
+++ bubbles.js
@@ -1,8 +1,11 @@
 var rawHexR = "6789ABCD"; // numbers to use as a basis for generating #RR----
 var rawHexG = "9ABCDE"; // numbers to use as a basis for generating #--GG--
 var rawHexB = "CDEF"; // numbers to use as a basis for generating #----BB
+var xycoords = [-10, -9, -8, -7, -6, -5, -4, -3, -2, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var zcoords = [-10, -9, -8, -7, -6, -5, -4, -3, 3, 4, 5, 6, 7, 8, 9, 10];
 
-/* simple start sphere with text */
+/* simple start sphere with text 
+ when popped, starts bubbles spawning */
 AFRAME.registerComponent('startbubble', {
 	
 	schema: {
@@ -21,9 +24,9 @@ AFRAME.registerComponent('startbubble', {
 		
 		el.setObject3D('mesh', this.mesh);
 		
-		/* on click, play 'pop' sound and remove this component */
+		/* on click, start adding more bubbles, play pop sound, remove component */
 		el.addEventListener('click', function() {
-			setInterval(createBubbles, 3000);
+			setInterval(createBubbles, 2000);
 			popSound.components.sound.playSound();
 			el.parentNode.removeChild(el);
 			
@@ -63,6 +66,7 @@ AFRAME.registerComponent('bubble', {
 		el.addEventListener('click', function() {
 			
 			popSound.components.sound.playSound();
+			
 			el.parentNode.removeChild(el);
 			
 		});
@@ -102,10 +106,11 @@ function getRandomColor(){
 function getRandomPosition(){
 	
 	// generate x, y, z coordinates
-	var x = (Math.floor((Math.random() * 20))) - 10;
-	var y = (Math.floor((Math.random() * 20))) - 10;
-	var z = (Math.floor((Math.random() * 20))) - 10;
+	var x = xycoords[Math.floor((Math.random() * 18))];
+	var y = xycoords[Math.floor((Math.random() * 18))];
+	var z = zcoords[Math.floor((Math.random() * 16))];
 
+	console.log(x, y, z);
 	return {x, y, z};	
 	
 }
